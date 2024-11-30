f85cb9c299c93ac617d5d6be4ab7650f51204ad5
Mohammed <mehmedkhan16@gmail.com>
2024-11-30 15:32:33 +0530

    grpc clinet added

--- ecomm/handlers.py
+++ ecomm/handlers.py
@@ -2,5 +2,5 @@ from django_socio_grpc.services.app_handler_registry import AppHandlerRegistry
 from .views import ProductService
 
 def grpc_handlers(server):
-    app_registry = AppHandlerRegistry("quickstart", server)
+    app_registry = AppHandlerRegistry("ecomm", server)
     app_registry.register(ProductService)
